429406a8865a9aacf2815210f457a83b8b144ff7
Jakub Czyz <jakub12134@gmail.com>
2023-09-22 16:00:13 +0200

    remove obsolete files

--- scripts/generate-models.sh
+++ scripts/generate-models.sh
@@ -1,15 +1,3 @@
-# # Run the openapi-generator Docker image to generate the client SDK
-# docker run --rm \
-#   -v "./swagger.json:/local/openapi.json" \
-#   -v "./models2:/local/out" \
-#   openapitools/openapi-generator-cli:v5.3.0 \
-#   generate \
-#   -i /local/openapi.json \
-#   -g go \
-#   -o /local/out \
-#   --skip-validate-spec \
-#   --global-property "models" \
-#   --additional-properties="enumClassPrefix=true,isGoSubmodule=true,packageName=models" \
-
-swagger generate model -m models 
+swagger generate model -f $1 -m models
+rm models/wrapped_*.go
 sed -i ':a;N;$!ba;s/struct {\n\t\t\([a-zA-Z]*\)\n\t}/\1/g' models/*.go
